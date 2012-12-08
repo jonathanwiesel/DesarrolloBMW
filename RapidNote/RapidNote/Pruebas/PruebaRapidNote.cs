06e8b985691012a4bb5bf37aafafcc9c28f3dc01
elbano marquez <elbano28@gmail.com>
2012-12-08 16:34:22 -0430

    subiendo clase de pruebas con su namespace

---
+++ RapidNote/RapidNote/Pruebas/PruebaRapidNote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+
+namespace RapidNote.Pruebas
+{
+    [TestFixture]
+    public class PruebaRapidNote
+    {
+
+    }
+}
